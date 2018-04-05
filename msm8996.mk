db1a1cc678d6983721eaa76d8c127f7e2f58b008
gabr0 <gabronogue@gmail.com>
2018-04-05 20:36:10 +0200

    msm8996: Add kcal and fix related denials
    msm8996: Add kcal and fix related denials

--- msm8996.mk
+++ msm8996.mk
@@ -171,6 +171,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.1 \
     vendor.display.config@1.1_vendor
 
+#KCAL
+PRODUCT_PACKAGES += \
+    kcal
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
