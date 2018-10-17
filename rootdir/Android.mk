zuk-devs/android_device_zuk_msm8996-common

msm8996-common: Use MIUI poweroff charging animation
 * It's cool, and more importantly... it actually works!
   Oh look, it even displays battery level. Wow. :-P

rootdir/Android.mk
@@ -78,3 +78,13 @@ LOCAL_MODULE_CLASS 	:= ETC
 LOCAL_SRC_FILES    	:= etc/init.qcom.sensors.sh
 LOCAL_MODULE_PATH  	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE          := chargeonlymode
+LOCAL_MODULE_OWNER    := zuk
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/chargeonlymode
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
+include $(BUILD_PREBUILT)

Gabronog/device_leeco_msm8996-common

Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

rootdir/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE          := chargeonlymode
+LOCAL_MODULE_OWNER    := leeco
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/chargeonlymode
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
+include $(BUILD_PREBUILT)